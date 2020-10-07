4a532e24ecb9143cb3c56280ee3ff39ced75087a
David Bamat <dbamat@edc.org>
2020-10-06 21:10:58 -0400

    Two files: 1) Syntax file for creating a modified version of the example NAEP data set from the EdSurvey package and 2) the modified data set.

---
+++ Setting up a modified version of the EdSurvey example (artificial) data.R
@@ -0,0 +1,37 @@
+#####################################################################################
+### Setting up a modified version of the EdSurvey example (artificial) NAEP data ####
+#####################################################################################
+
+###Load packages###
+
+#By loading "tidyverse," a bunch of popular R packages are loaded-- including dplyr and ggplot2
+if(!require(tidyverse)) install.packages("tidyverse", 
+                                         repos = "http://cran.us.r-project.org")
+
+#for the example data
+if(!require(EdSurvey)) install.packages("EdSurvey", 
+                                        repos = "http://cran.us.r-project.org")
+
+
+###Unpack artificial data set from EdSurvey package###
+
+#import data and save to object named "df"
+df <- readNAEP(system.file("extdata/data", "M36NT2PM.dat", package = "NAEPprimer"))
+
+#reformat data into a data frame
+df <- getData(df, c('composite', 'scrpsu', 'pared', 'sdracem', 'lep'), 
+              omittedLevels = FALSE, addAttributes = TRUE)
+
+#perform some data wrangling to ready data for analysis
+df <- df %>%
+  mutate(college = if_else(pared == "Graduated college", 1, 0),
+         white = if_else(sdracem == "White", 1, 0),
+         el = if_else(lep == "Yes", 1, 0)) %>%
+  select("scrpsu", "mrpcm1", "college", "white", "el")
+
+names(df) <- c("school", "score", "college", "white", "el")
+
+###Export data as .csv###
+write.csv(df,  "~/R/EdSurvey_demo/artificial-NAEP-data.csv")
+
+
